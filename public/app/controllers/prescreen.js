4164c036f53a8e15e96adb186704385e01918bb1
Samuel Christensen <samachr@gmail.com>
2016-03-05 14:41:09 -0700

    removed double requests

--- public/app/controllers/prescreen.js
+++ public/app/controllers/prescreen.js
@@ -1,22 +1,18 @@
 lb.controller("prescreen", function($scope, $http, $location){
   $scope.error = "";
 
+
   $http.get("/api/v1/survey").then(function(data, status) {
-    console.log(data.data.questionList);
     $scope.questionList = data.data.questionList;
+    $scope.juror_id = data.data.juror_id || 3;
    })
 
   $scope.submit = function() {
     $scope.error = "";
 
-    var data = {juror_id: 0, responses: $scope.answers}
-    // var data = {responses: Object.keys($scope.answers).map(function(questionID){
-    //   return {questionID: questionID, ResponseValue: $scope.answers[questionID]}
-    // }),
-
-    $http.post("/api/v1/survey", data).success(function(data, status) {
+    var data = {juror_id: $scope.juror_id, responses: $scope.answers}
+    $http.post("/api/v1/survey", data).then(function(data, status) {
         $location.path("/registration");
      })
-
   }
 })
